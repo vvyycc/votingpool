71f1f1df6d7fa6909021c1f2b0b1cf29379f8e5c
Victor <victorroldan18@gmail.com>
2022-07-26 14:11:11 +0200

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -22,44 +22,13 @@ describe("Core", function () {
 
   it("Creating a votingpoll", async () => {
 
-    // ContractFactory in ethers.js is an abstraction used to deploy new smart contracts, so Token here is a factory for instances of our token contract.
    
-
-   
-    // deploy votingPoll factory
-    
-
-    // Create one votingPoll selecting one options with first address
-
-    // Check if the creator can vote
-
-    // Vote with another address
-
-    // Check if the account that voted can vote
-
-    // call function with (address owner, uint256 number) parameters
-    // let arrayVotingPollF = new Array<any>()
     
      receptTx = await contract.createVotingPoll("Hola",ownerAddress,'Good');
      countVote++;
      
     console.log(receptTx);
     
-  //  const votingPoll=await  contract.getVotingPollF(countVote)
-    // const numVotesInDifferentOptionsByVotingPoll=   await contract.getVotingPollOptions(countVote,'Good')
-    // console.log(numVotesInDifferentOptionsByVotingPoll);
-    // console.log(owner.address);
-    // console.log(votingPoll);
-    // const count = await contract.votingPollCount();
-    // for(let i=1;i <= Number(count);i++){
-    //   arrayVotingPollF.push(contract.getVotingPollF(i))
-    // }
-    // expect(arrayVotingPollF).to.be.a('array');
-    // expect(arrayVotingPollF).to.have.lengthOf.above(0);
-    // const tx2_receipt = await vf.createVotingPoll("que tal","0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199",'Bad');
-    // const tx3_receipt = await vf.createVotingPoll("estamos","0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199",'Very good');
-
-    // console.log("VotingPollFactory address at: ", tx1_receipt.to);
 
 
  
@@ -68,9 +37,38 @@ describe("Core", function () {
 
 
   })
+  it("The Voter can´t create another VotingPoll", async () => {
+
+   
+    
+    await contract.createVotingPoll("Hola",ownerAddress,'Good');
+  
+    await expect(contract.createVotingPoll("Hola",ownerAddress,'Good')).to.be.revertedWith(
+      "Voter has no votes left."
+    );
+    
+
+   
+
+
+
+   console.log("\n")
+
+
+
+ })
   it("Vote a votingPoll that it was created", async () => {
     receptTx = await contract.createVotingPoll("Hola",ownerAddress,'Good');
     await contract.vote(voterAddress,1,'Good')
+    const count = await contract.votingPollCount();
+    console.log(count)
+  })
+  it("Voter has no votes left", async () => {
+    receptTx = await contract.createVotingPoll("Hola",ownerAddress,'Good');
+    await contract.vote(voterAddress,1,'Good')
+    await expect( contract.vote(voterAddress,1,'Good')).to.be.revertedWith(
+      "Voter has no votes left.");
+    
     const count = await contract.votingPollCount();
     console.log(count)
   })
